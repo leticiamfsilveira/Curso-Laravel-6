26aa738c69ea6886a9aa89af35b061b25cb04e5c
lesfernandes <lemayara16@gmail.com>
2020-01-22 11:57:27 -0300

    criando a tabela stores

---
+++ marketplace_l6/database/migrations/2020_01_22_143835_create_table_store.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableStore extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->string('slug');
+
+            $table->timestamps();
+
+            //Definição da chave estrangeira
+            $table->foreign('user_id')->references('id')->on('users');
+            //stores_user_id_foreign (formatação para criação de chave estrangeira na tabela)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}

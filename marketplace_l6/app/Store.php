33f767147b08525f23acfe875699b2320d998041
lesfernandes <lemayara16@gmail.com>
2020-02-17 13:41:28 -0300

    Criando o relacionamento um para um de Stores e User

--- marketplace_l6/app/Store.php
+++ marketplace_l6/app/Store.php
@@ -9,5 +9,8 @@ class Store extends Model
     // define o nome da tabela caso seja diferente da conevção do laravel que é o plural do nome do model
     // protected $table = 'lojas';
 
+    public function user(){
+        $this->belongsTo(User::class);
+    }
 
 }

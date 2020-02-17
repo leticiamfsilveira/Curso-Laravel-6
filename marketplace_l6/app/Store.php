6be9a210cdf706f17c4fdeab1a863e9322b72a6b
lesfernandes <lemayara16@gmail.com>
2020-02-17 17:28:43 -0300

    criando seeds

--- marketplace_l6/app/Store.php
+++ marketplace_l6/app/Store.php
@@ -8,6 +8,7 @@ class Store extends Model
 {
     // define o nome da tabela caso seja diferente da conevção do laravel que é o plural do nome do model
     // protected $table = 'lojas';
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
 
     public function user(){
         return $this->belongsTo(User::class);

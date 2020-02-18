2415caa8036fc9f44ca7ea5d088f821329df74c7
lesfernandes <lemayara16@gmail.com>
2020-02-18 15:35:31 -0300

    criação de rotas e views de lojas

---
+++ marketplace_l6/resources/views/admin/stores/create.blade.php
@@ -0,0 +1,35 @@
+<h1>Criar Loja</h1>
+<form action="/admin/stores/store" method="post">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div>
+        <label>Loja</label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label>Descrição</label>
+        <input type="text" name="description">
+    </div>
+    <div>
+        <label>Telefone</label>
+        <input type="text" name="phone">
+    </div>
+    <div>
+        <label>Celular/Whatsapp</label>
+        <input type="text" name="mobile_phone">
+    </div>
+    <div>
+        <label>Slug</label>
+        <input type="text" name="slug">
+    </div>
+    <div>
+        <label>Usuário</label>
+        <select name="user">
+            @foreach($users as $user)
+                <option value="{{$user->id}}">{{$user->name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <button type="submit">Criar Loja</button>
+    </div>
+</form>

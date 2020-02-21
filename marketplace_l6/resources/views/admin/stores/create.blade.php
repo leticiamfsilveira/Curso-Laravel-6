da7cb5e2a4f3ce7de3261f4f938adbe1fcff3cf9
lesfernandes <lemayara16@gmail.com>
2020-02-21 14:43:34 -0300

    recuperando dados digitados pelo usuário

--- marketplace_l6/resources/views/admin/stores/create.blade.php
+++ marketplace_l6/resources/views/admin/stores/create.blade.php
@@ -7,7 +7,8 @@
 
         <div class="form-group">
             <label>Loja</label>
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                   value="{{old('name')}}">
             @error('name')
             <div class="invalid-feedback">
                 {{$message}}
@@ -17,7 +18,8 @@
 
         <div class="form-group">
             <label>Descrição</label>
-            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror"
+                   value="{{old('description')}}">
             @error('description')
             <div class="invalid-feedback">
                 {{$message}}
@@ -27,7 +29,8 @@
 
         <div class="form-group">
             <label>Telefone</label>
-            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror"
+                   value="{{old('phone')}}">
             @error('phone')
             <div class="invalid-feedback">
                 {{$message}}
@@ -37,7 +40,8 @@
 
         <div class="form-group">
             <label>Celular/Whatsapp</label>
-            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror"
+                   value="{{old('mobile_phone')}}">
             @error('mobile_phone')
             <div class="invalid-feedback">
                 {{$message}}
@@ -47,7 +51,8 @@
 
         <div class="form-group">
             <label>Slug</label>
-            <input type="text" name="slug" class="form-control @error('slug') is-invalid @enderror">
+            <input type="text" name="slug" class="form-control @error('slug') is-invalid @enderror"
+                   value="{{old('slug')}}">
             @error('slug')
             <div class="invalid-feedback">
                 {{$message}}

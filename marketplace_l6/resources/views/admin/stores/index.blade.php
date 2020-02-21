fb897c6885d1b723a440bd3c05368f843b521fb7
lesfernandes <lemayara16@gmail.com>
2020-02-21 15:23:44 -0300

    iniciando controle de lojas por usuários

--- marketplace_l6/resources/views/admin/stores/index.blade.php
+++ marketplace_l6/resources/views/admin/stores/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar loja</a>
+    @if(!$store)
+        <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar loja</a>
+    @endif
     <table class="table table-striped">
         <thead>
             <tr>
@@ -11,24 +13,21 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($stores as $store)
-                <tr>
-                    <td>{{$store->id}}</td>
-                    <td>{{$store->name}}</td>
-                    <td>
-                        <div class="btn-group">
-                            <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
-                            <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="post">
-                                @csrf
-                                @method("DELETE")
-                                <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
+            <tr>
+                <td>{{$store->id}}</td>
+                <td>{{$store->name}}</td>
+                <td>
+                    <div class="btn-group">
+                        <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
+                        <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="post">
+                            @csrf
+                            @method("DELETE")
+                            <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
         </tbody>
     </table>
 
-    {{$stores->links()}}
 @endsection

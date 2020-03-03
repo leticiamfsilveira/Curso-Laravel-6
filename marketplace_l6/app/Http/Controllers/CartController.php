33dc6ebcc709b75a9459b47e5c0e5befb34681f0
lesfernandes <lemayara16@gmail.com>
2020-03-03 15:48:27 -0300

    tela do carrinho de compras

--- marketplace_l6/app/Http/Controllers/CartController.php
+++ marketplace_l6/app/Http/Controllers/CartController.php
@@ -9,8 +9,11 @@ class CartController extends Controller
 
     public function index()
     {
-        dd(session()->get('cart'));
+        $cart = session()->has('cart') ? session()->get('cart') : [];
+
+        return view('cart', compact('cart'));
     }
+
     public function add(Request $request)
     {
         $product = $request->get('product');

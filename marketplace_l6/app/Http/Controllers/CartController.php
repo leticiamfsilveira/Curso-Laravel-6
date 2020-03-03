3fa24fc55d691099f30db1fa532fccfa7e7ec045
lesfernandes <lemayara16@gmail.com>
2020-03-03 12:44:14 -0300

    adição de produto no carrinho

---
+++ marketplace_l6/app/Http/Controllers/CartController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+
+    public function index()
+    {
+        dd(session()->get('cart'));
+    }
+    public function add(Request $request)
+    {
+        $product = $request->get('product');
+
+        //verificar se existe sessão para os produtos
+        if(session()->has('cart')) {
+            //existindo, adicionar este produto na sessão existente
+            session()->push('cart', $product);
+        } else {
+            //não existindo, criar a sessão com o primeiro produto
+            $products[] = $product;
+
+            session()->put('cart', $products);
+        }
+
+        flash('Producto Adicionado no Carrinho.')->success();
+        return redirect()->route('product.single', ['slug' => $product['slug']]);
+    }
+}

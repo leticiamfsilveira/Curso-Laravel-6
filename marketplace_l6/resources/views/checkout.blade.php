04330d956597ff73b321cc8d27d68591ba457213
lesfernandes <lemayara16@gmail.com>
2020-03-18 13:22:08 -0300

    iniciando requisição de pagamento

--- marketplace_l6/resources/views/checkout.blade.php
+++ marketplace_l6/resources/views/checkout.blade.php
@@ -50,7 +50,7 @@
 
 @section('scripts')
     <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
-
+    <script src="{{asset('assets/js/jquery.ajax.js')}}"></script>
     <script>
         const sessionId = '{{session()->get('pagseguro_session_code')}}';
 
@@ -94,9 +94,30 @@
                 expirationYear: document.querySelector('input[name=card_year]').value,
                 success: function (res) {
                     console.log(res);
+
+                    processPayment(res.card.token);
                 }
             });
         });
+
+        function processPayment() {
+
+            let data = {
+                token: token,
+                hash: PagSeguroDirectPayment.getSenderHash(),
+                installment: document.querySelector('select_installments').value
+            };
+
+            $.ajax({
+                type: 'post',
+                url: '',
+                data: data,
+                dataType: 'json',
+                success: function (res) {
+                    console.log(res);
+                }
+            });
+        }
 
         function getInstallments(amount, brand) {
             PagSeguroDirectPayment.getInstallments({
@@ -119,7 +140,7 @@
         function drawSelectInstallments(installments) {
             let select = '<label>Opções de Parcelamento:</label>';
 
-            select += '<select class="form-control">';
+            select += '<select class="form-control select_installments">';
 
             for(let l of installments) {
                 select += `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x de ${l.installmentAmount} - Total fica ${l.totalAmount}</option>`;
@@ -131,4 +152,5 @@
             return select;
         }
     </script>
+
 @endsection

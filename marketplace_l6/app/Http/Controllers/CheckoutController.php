61d9ed4fbacaebe884b7b2c68e55782d78c2d283
lesfernandes <lemayara16@gmail.com>
2020-03-19 14:09:53 -0300

    efetuando pagamento SDK

--- marketplace_l6/app/Http/Controllers/CheckoutController.php
+++ marketplace_l6/app/Http/Controllers/CheckoutController.php
@@ -21,7 +21,95 @@ class CheckoutController extends Controller
 
     public function proccess(Request $request)
     {
-        dd($request->all());
+        $dataPost = $request->all();
+
+        $reference = 'XPTO';
+
+        $creditCard = new \PagSeguro\Domains\Requests\DirectPayment\CreditCard();
+
+        $creditCard->setReceiverEmail(env('PAGSEGURO_EMAIL'));
+        $creditCard->setReference($reference);
+        $creditCard->setCurrency("BRL");
+
+        $cartItems = session()->get('cart');
+
+        foreach ($cartItems as $item) {
+            $creditCard->addItems()->withParameters(
+                $reference,
+                $item['name'],
+                $item['amount'],
+                $item['price']
+            );
+        }
+
+        $user = auth()->user();
+        $email = env('PAGSEGURO_ENV') == 'sandbox' ? 'text@sandbox.pagseguro.com.br' : $user->email;
+
+        $creditCard->setSender()->setName($user->name);
+        $creditCard->setSender()->setEmail($email);
+
+        $creditCard->setSender()->setPhone()->withParameters(
+            11,
+            56273440
+        );
+
+        $creditCard->setSender()->setDocument()->withParameters(
+            'CPF',
+            '70822819783'
+        );
+
+        $creditCard->setSender()->setHash($dataPost['hash']);
+
+        $creditCard->setSender()->setIp('127.0.0.0');
+
+        $creditCard->setShipping()->setAddress()->withParameters(
+            'Av. Brig. Faria Lima',
+            '1384',
+            'Jardim Paulistano',
+            '01452002',
+            'São Paulo',
+            'SP',
+            'BRA',
+            'apto. 114'
+        );
+
+        $creditCard->setBilling()->setAddress()->withParameters(
+            'Av. Brig. Faria Lima',
+            '1384',
+            'Jardim Paulistano',
+            '01452002',
+            'São Paulo',
+            'SP',
+            'BRA',
+            'apto. 114'
+        );
+
+        $creditCard->setToken($dataPost['card_token']);
+
+        list($quantity, $installmentAmount) = explode('|', $dataPost['installment']);
+
+        $creditCard->setInstallment()->withParameters($quantity, $installmentAmount);
+
+        $creditCard->setHolder()->setBirthdate('01/10/1979');
+        $creditCard->setHolder()->setName($dataPost['card_name']);
+
+        $creditCard->setHolder()->setPhone()->withParameters(
+            11,
+            56273440
+        );
+
+        $creditCard->setHolder()->setDocument()->withParameters(
+            'CPF',
+            '70822819783'
+        );
+
+        $creditCard->setMode('DEFAULT');
+
+        $result = $creditCard->register(
+            \PagSeguro\Configuration\Configure::getAccountCredentials()
+        );
+
+        var_dump($result);
     }
 
     private function makePagSeguroSession()

16e839e1fe45c37dec0a70a87f68a4cea8c844a5
lesfernandes <lemayara16@gmail.com>
2020-03-20 14:20:53 -0300

    melhorias home

--- marketplace_l6/resources/views/welcome.blade.php
+++ marketplace_l6/resources/views/welcome.blade.php
@@ -23,4 +23,22 @@
             @if(($key + 1) % 3 == 0) </div> <div class="row front"> @endif
         @endforeach
     </div>
+
+    <div class="row">
+        <div class="col-12">
+            <h2>Lojas Destaque</h2>
+            <hr>
+        </div>
+        @foreach($stores as $store)
+            <div class="col-4">
+                @if($store->logo)
+                    <img src="{{asset('storage/'.$store->logo)}}" alt="Logo da Loja {{$store->name}}" class="img-fluid">
+                @else
+                    <img src="https://via.placeholder.com/600X300.png?text=logo" alt="Loja sem Logo" class="img-fluid">
+                @endif
+                <h3>{{$store->name}}</h3>
+                <p>{{$store->description}}</p>
+            </div>
+        @endforeach
+    </div>
 @endsection

65023207fb6f1700ec5a0889ed7bfe4f52485e84
lesfernandes <lemayara16@gmail.com>
2020-03-26 15:19:07 -0300

    corrigindo adição no carrinho

--- marketplace_l6/app/Http/Controllers/CartController.php
+++ marketplace_l6/app/Http/Controllers/CartController.php
@@ -16,7 +16,14 @@ class CartController extends Controller
 
     public function add(Request $request)
     {
-        $product = $request->get('product');
+        $productData = $request->get('product');
+
+        $product = \App\Product::whereSlug($productData['slug']);
+
+        if(!$product->count() || $productData['amount'] == 0)
+            return redirect()->route('home');
+
+        $product = array_merge($productData, $product->first(['name', 'price'])->toArray());
 
         //verificar se existe sessão para os produtos
         if(session()->has('cart')) {
